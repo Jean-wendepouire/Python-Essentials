20ad0009771ba13a1b75323b40a708d4327728cc
Jean-wendepouire <wendepouire@gmail.com>
2025-08-21 15:10:07 +0000

    update hello.py and create print_func.py

---
+++ Module2/print_func.py
@@ -0,0 +1,42 @@
+print("Hello, Python!!!")
+
+#print("John")
+#print(John)
+#print"John"
+#print('John')
+#print("John") print("Python")
+
+# Insert newline
+print("The itsy bitsy spider climbed up the waterspout.")
+print()
+print("Down came the rain and washed the spider out.\n")
+
+print("The itsy bitsy spider\nclimbed up the waterspout.")
+print()
+print("Down came the rain\nand washed the spider out.")
+
+# Multiple Arguments
+print("The itsy bitsy spider", "climbed up", "the waterspout.")
+
+# Keywords Arguments
+print("My name is", "Python.", end=" ")
+print("Monty Python")
+
+print("My name is ", end="")
+print("Monty Python")
+
+print("My", "name", "is", "Monty", "Python", sep="-")
+print("My", "name", "is", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
+
+print("Programming","Essentials","in", sep="***", end="...")
+print("Python")
+
+print("     *")
+print("    * *")
+print("   *   *")
+print("  *     *")
+print(" ***    ***")
+print("   *    *")
+print("   *    *")
+print("   ******")
